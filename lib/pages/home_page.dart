d0232a56882b498a79feb7b62cf3410dcbb14546
dafm10 <dafm.10@gmail.com>
2022-01-26 21:10:24 -0500

    home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo4_partidop/ui/general/colors.dart';
+import 'package:flutter_codigo4_partidop/ui/widgets/item_list_home_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -22,47 +23,22 @@ class HomePage extends StatelessWidget {
           SizedBox(
             height: _height * 0.01,
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12.0),
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              color: COLOR_BRAND_PRIMARY,
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff0D48C1),
-                  blurRadius: 12,
-                  offset: Offset(4,4),
-                ),
-              ],
-            ),
-            child: ListTile(
-              // minLeadingWidth para el espacio entre los items
-              minLeadingWidth: 0,
-              contentPadding: EdgeInsets.zero,
-              leading: SvgPicture.asset(
-                "assets/images/bx-male-female.svg",
-                color: Colors.white,
-                height: 34.0,
-              ),
-              title: Text(
-                "Candidatos al congreso",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Contamos con un equipo de verdad ¡Conócelos!",
-                style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 13.0
-                ),
-              ),
-              trailing: SvgPicture.asset(
-                "assets/images/bx-chevron-right.svg",
-                color: Colors.white,
-                height: 34.0,
-              ),
-            ),
+          ItemListHomeWidget(
+            title: "Candidatos al Congreso",
+            subTitle: "Contamos con un equipo de verdad, ¡Conócelos!",
+            imageLeading: "assets/images/bx-male-female.svg",
+            onPressed: () {},
           ),
+          ItemListHomeWidget(
+              title: "Propiuestas Moradas",
+              subTitle: "Propuestas Legislativas - Congreso 2020",
+              imageLeading: "assets/images/bx-spread.svg",
+              onPressed: () {}),
+          ItemListHomeWidget(
+              title: "Bases Moradas",
+              subTitle: "Descrube la base morada más cerca de ti",
+              imageLeading: "assets/images/bx-map.svg",
+              onPressed: () {}),
         ],
       ),
     );
