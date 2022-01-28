e11bec4ba041f77795851e9a3b47c204ab178094
dafm10 <dafm.10@gmail.com>
2022-01-27 21:14:08 -0500

    candidate detail page 2

--- lib/pages/candidate_detail_page.dart
+++ lib/pages/candidate_detail_page.dart
@@ -1,16 +1,51 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo4_partidop/ui/general/colors.dart';
 
 class CandidateDetailPage extends StatelessWidget {
   const CandidateDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Example"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: COLOR_BRAND_PRIMARY,
+            expandedHeight: _height * 0.4,
+            elevation: 0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Hola"),
+              centerTitle: true,
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    "http://192.168.1.37:8000/media/candidatos/fracisco_sagasti.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 300,
+                  color: Colors.redAccent,
+                ),
+                Container(
+                  height: 400,
+                  color: Colors.blueAccent,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(),
     );
   }
 }
