0c13f08cc9790cae5adbc9fbf1bc66f5f616fc00
dafm10 <dafm.10@gmail.com>
2022-01-27 21:37:34 -0500

    candidate detail page 5

--- lib/pages/candidate_detail_page.dart
+++ lib/pages/candidate_detail_page.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_codigo4_partidop/ui/general/colors.dart';
 
 class CandidateDetailPage extends StatefulWidget {
-
   @override
   State<CandidateDetailPage> createState() => _CandidateDetailPageState();
 }
 
-class _CandidateDetailPageState extends State<CandidateDetailPage>  with SingleTickerProviderStateMixin{
-
+class _CandidateDetailPageState extends State<CandidateDetailPage>
+    with SingleTickerProviderStateMixin {
   TabController? _tabController;
 
   @override
@@ -24,67 +23,61 @@ class _CandidateDetailPageState extends State<CandidateDetailPage>  with SingleT
     double _width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: COLOR_BRAND_PRIMARY,
-            expandedHeight: _height * 0.4,
-            elevation: 0,
-            floating: false,
-            pinned: true,
-            snap: false,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text("Hola"),
-              centerTitle: true,
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  Image.network(
-                    "http://192.168.1.37:8000/media/candidatos/fracisco_sagasti.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                  DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.center,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          COLOR_BRAND_PRIMARY.withOpacity(0),
-                          COLOR_BRAND_PRIMARY.withOpacity(0.5),
-                        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              backgroundColor: COLOR_BRAND_PRIMARY,
+              expandedHeight: _height * 0.4,
+              elevation: 0,
+              floating: true,
+              pinned: true,
+              snap: false,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Holass"),
+                centerTitle: true,
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      "http://192.168.1.37:8000/media/candidatos/fracisco_sagasti.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.center,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            COLOR_BRAND_PRIMARY.withOpacity(0),
+                            COLOR_BRAND_PRIMARY.withOpacity(0.5),
+                          ],
+                        ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Container(
+                    height: 300,
+                    color: Colors.redAccent,
+                  ),
+                  Container(
+                    height: 400,
+                    color: Colors.blueAccent,
                   ),
                 ],
               ),
             ),
-            bottom: TabBar(
-              controller: _tabController,
-              tabs: [
-                Tab(
-                  text: "Hola",
-                ),
-                Tab(
-                  text: "Hola",
-                ),
-              ],
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Container(
-                  height: 300,
-                  color: Colors.redAccent,
-                ),
-                Container(
-                  height: 400,
-                  color: Colors.blueAccent,
-                ),
-              ],
-            ),
-          ),
-        ],
+          ];
+        },
+        body: Center(
+          child: Text("Hola"),
+        ),
       ),
     );
   }
